88c96b7d071546d407160c883e63dec081102e90
sopip <162905162+sopip@users.noreply.github.com>
2025-04-08 15:57:41 +0200

    vent skal lige rydde op

--- ddu-eksamensprojekt/Scripts/player_2.gd
+++ ddu-eksamensprojekt/Scripts/player_2.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @onready var timer: Timer = $Timer
 @export var speed: float = 150
+@export var shootCooldown := 1.0
 @onready var global = get_node("/root/Global")
 
 var player_id = 2
@@ -26,7 +27,7 @@ func _process(delta):
 
 	velocity = direction * speed
 	move_and_slide()
-
+	
 	#her skyder spilleren
 	if Input.is_action_just_pressed("Space") and canShoot:
 		shoot()
@@ -38,11 +39,11 @@ func _process(delta):
 func shoot():
 	print("player " , player_id , " skyder")
 	canShoot = false
-	timer.start()
+	timer.start(shootCooldown)
 
 	var bullet = preload("res://scenes/ball.tscn").instantiate()
 	bullet.position = position
-	bullet.owner_id = 2
+	bullet.owner_id = 1
 	bullet.direction = ballDirection.normalized()#Retningen kuglen skal flyve i
 	get_parent().add_child(bullet)
 
@@ -52,6 +53,6 @@ func _on_timer_timeout() -> void:
 func take_damage(amount: int):
 	global.hit2 += amount
 	print("Player 2 blev ramt! Hits: " + str(global.hit2))
-	if global.hit2 >= 3:
-		print("spiller " , player_id , " er død")
+	if global.hit2 >= global.max_hits:
+		print("spiller" + player_id + "er død")
 		#die() der skal laves en funktion der dræber spilleren
