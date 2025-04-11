0f391ed7e50da0e93e407484da5ca08dbae542a3
sopop <162905162+sopip@users.noreply.github.com>
2025-04-11 15:22:25 +0200

    ved ik hvad der er galt med skriften 😇

--- ddu-eksamensprojekt/scripts/next_lvl.gd
+++ ddu-eksamensprojekt/scripts/next_lvl.gd
@@ -5,6 +5,8 @@ var started = false
 var stage = 0 #de lodrette levels. dem er der 3 af (eller måske kun 2 i vores spil)
 var level = 3 #her er de vandrette levels. der er 5 levels pr. stage Jeg sætter den i midten til 3 så det måske er nememre at skifte.. ved ik om der vil være problemer med -
 const FILE_BEGIN = "res://scenes/level"
+#labels
+@onready var vinder = $Panel/Label
 
 func _process(delta: float) -> void:
 	#her skiftes banerne og der sker animation.
@@ -15,6 +17,7 @@ func _process(delta: float) -> void:
 			Global.hit1 = 0
 			Global.hit2 = 0
 			started = true
+			vinder.text = "player 2 har vundet"
 			animation.play("ani") #i parenteset hedder den Animation (når man trykker på animationen og kigger ende på navnet!
 		elif Global.hit2>=3:
 			#her vinder player 1
@@ -22,6 +25,7 @@ func _process(delta: float) -> void:
 			Global.hit1 = 0
 			Global.hit2 = 0
 			started = true
+			vinder.text = "player 1 har vundet"
 			animation.play("ani")
 
 #bane skifter
