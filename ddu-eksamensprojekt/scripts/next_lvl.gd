49e7de06ecdcf9ac080e5fdd2c34c47e4dc60a1c
sopop <162905162+sopip@users.noreply.github.com>
2025-04-11 14:48:59 +0200

    småfejl inden jeg prøver winningscreen igen

--- ddu-eksamensprojekt/scripts/next_lvl.gd
+++ ddu-eksamensprojekt/scripts/next_lvl.gd
@@ -6,6 +6,9 @@ var stage = 0 #de lodrette levels. dem er der 3 af (eller måske kun 2 i vores s
 var level = 3 #her er de vandrette levels. der er 5 levels pr. stage Jeg sætter den i midten til 3 så det måske er nememre at skifte.. ved ik om der vil være problemer med -
 const FILE_BEGIN = "res://scenes/level"
 
+func _ready() -> void:
+	print("HEEJEJJEJEJJEJEJ")
+
 func nextLvl():
 	#her skiftes banerne og der sker animation.
 	if started == false:
