501c396bffc5a9ec6985592d51ad5510bbf380d7
sopop <162905162+sopip@users.noreply.github.com>
2025-04-13 18:39:06 +0200

    is speed

--- ddu-eksamensprojekt/scripts/map_objects/icecream.gd
+++ ddu-eksamensprojekt/scripts/map_objects/icecream.gd
@@ -1,8 +1,9 @@
 extends Area2D
+#fungerer bare som ketchup men er sat i sin egen scene alligevel
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		body.take_slow(100)
+		body.take_slow(200)
 		print("player slowed")
 
 func _on_body_exited(body: Node2D) -> void:
