40384ef97e2e5336ccbf8e6dc432d4641e248055
sopop <162905162+sopip@users.noreply.github.com>
2025-05-01 14:16:17 +0200

    småting

--- ddu-eksamensprojekt/Scripts/menu.gd
+++ ddu-eksamensprojekt/Scripts/menu.gd
@@ -1,13 +1,15 @@
 extends Button
 
+@onready var pop = $PopupMenu
+
 func _on_pressed():
-	$PopupMenu.show()
+	pop.show()
 
 #id er deres array plads ting
 func _on_popup_menu_id_pressed(id):
 	match id:
 		0: #fortsæt
-			$PopupMenu.hide()
+			pop.hide()
 		1: #Startskærm
 			get_tree().change_scene_to_file("res://scenes/startscreen.tscn")
 		2: #luk spil
