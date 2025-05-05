975b22b0a41aa6e04cc0bce7572342c737b97fbd
sopop <162905162+sopip@users.noreply.github.com>
2025-05-05 23:25:40 +0200

    intruktioner på is og ketchup

--- ddu-eksamensprojekt/scripts/map_objects/icecream.gd
+++ ddu-eksamensprojekt/scripts/map_objects/icecream.gd
@@ -1,10 +1,15 @@
 extends Area2D
 #fungerer bare som ketchup men er sat i sin egen scene alligevel
 
+func _ready() -> void:
+	if(get_tree().current_scene.scene_file_path == "res://scenes/level3.tscn"):
+		$Instructions.show()
+
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		body.take_slow(300)
 		print("player slowed")
+		$Instructions.hide()
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
--- ddu-eksamensprojekt/scripts/map_objects/ketchup.gd
+++ ddu-eksamensprojekt/scripts/map_objects/ketchup.gd
@@ -1,9 +1,14 @@
 extends Area2D
 
+func _ready() -> void:
+	if(get_tree().current_scene.scene_file_path == "res://scenes/level3.tscn"):
+		$Instructions.show()
+
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		body.take_slow(100)
 		print("player slowed")
+		$Instructions.hide()
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
