455262990fbb3b94e9d79a4f38058871f5765ece
sopop <162905162+sopip@users.noreply.github.com>
2025-05-06 23:19:01 +0200

    Fikset levels. Havde ikke gemt det level den var på - det er en ret stor fejl som vi havde været for dovne til at opdage tidligere

--- ddu-eksamensprojekt/scripts/global.gd
+++ ddu-eksamensprojekt/scripts/global.gd
@@ -3,3 +3,5 @@ extends Node
 #Hit 1 er de skader player 1 har taget
 var hit1: int=3
 var hit2: int=3
+#den skal huske det level vi er på
+var level: int=3
--- ddu-eksamensprojekt/scripts/next_lvl.gd
+++ ddu-eksamensprojekt/scripts/next_lvl.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var animation: AnimationPlayer = $AnimatedSprite2D/AnimationPlayer
 var started = false
 var stage = 0 #de lodrette levels. dem er der 3 af (eller måske kun 2 i vores spil)
-var level = 3 #her er de vandrette levels. der er 5 levels pr. stage Jeg sætter den i midten til 3 så det måske er nememre at skifte.. ved ik om der vil være problemer med -
+#level er de vandrette levels. der er 5 levels pr. stage Jeg sætter den i midten til 3 så det måske er nememre at skifte.. ved ik om der vil være problemer med -
 const FILE_BEGIN = "res://scenes/level"
 @onready var animated_sprite = $AnimatedSprite2D
 
@@ -12,13 +12,13 @@ func _process(_delta: float) -> void:
 	if started == false:
 		if Global.hit1<=0:
 			#her vinder player 2
-			level +=1
+			Global.level +=1
 			started = true
 			animated_sprite.play("winplayer2")
 			animation.play("ani") #i parenteset hedder den ani (når man trykker på animationen og kigger på navnet!
 		elif Global.hit2<=0:
 			#her vinder player 1
-			level -=1
+			Global.level -=1
 			started = true
 			animated_sprite.play("winplayer1")
 			animation.play("ani")
@@ -29,11 +29,13 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	Global.hit2 = 3
 	if get_tree().current_scene.scene_file_path == "res://scenes/tutorial.tscn":
 		get_tree().change_scene_to_file("res://scenes/startscreen.tscn")
+		Global.level = 3
 	else:
-		if level < 6 && level > 0:
+		if Global.level < 6 && Global.level > 0:
 			print("scenen skal nu skiftes til den næste scene")
-			var next_level_path = FILE_BEGIN + str(level) + ".tscn"
+			var next_level_path = FILE_BEGIN + str(Global.level) + ".tscn"
 			print(next_level_path)
 			get_tree().change_scene_to_file(next_level_path)
 		else:
 			get_tree().change_scene_to_file("res://scenes/startscreen.tscn")
+			Global.level = 3
