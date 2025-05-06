75a5fc58970c0f20a04b12cc4628d0c470cbfd75
Bucadebepo <77507575+Trismania@users.noreply.github.com>
2025-05-06 16:52:00 +0200

    Tekstboksen viser nu førvalgte farve

--- ddu-eksamensprojekt/scenes/SQLcontroller.gd
+++ ddu-eksamensprojekt/scenes/SQLcontroller.gd
@@ -6,6 +6,16 @@ func _ready() -> void:
 	database = SQLite.new()
 	database.path = "res://data.db"
 	database.open_db()
+	
+	
+	var result1 = database.select_rows("playerColor", "id = 1", ["color"])
+	if result1.size() > 0:
+		$Color1.text = result1[0]["color"]
+
+	
+	var result2 = database.select_rows("playerColor", "id = 2", ["color"])
+	if result2.size() > 0:
+		$Color2.text = result2[0]["color"]
 
 func _on_start_spil_button_down() -> void:
 	var color1 = $Color1.text
